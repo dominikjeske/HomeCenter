8f6f96d5201c73fcb5b5d0ea4d142e25119bdce9
dominik.jeske@hotmail.com <dominik.jeske@hotmail.com>
2018-07-18 22:53:16 +0200

    Final rename in files

--- Firmwares/HA4IoT.Controller.I2COutpost/LPD433MhzController.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-
-void LPD433MhzController_handleI2CWrite(uint8_t package[], uint8_t packageLength);
-uint8_t LPD433MhzController_handleI2CRead(uint8_t response[]);
-void LPD433MhzController_SetReadMode(uint8_t package[], uint8_t packageLength);
-void LPD433MhzController_loop();
-
