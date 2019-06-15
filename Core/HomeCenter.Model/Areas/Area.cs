3d868bf2379c2dd7acf77a9e679ab8bd1ad9ed5e
dnf <dominik.jeske@hotmail.com>
2019-06-15 16:18:50 +0200

    Stabilization

--- Core/HomeCenter.Model/Areas/Area.cs
+++ Core/HomeCenter.Model/Areas/Area.cs
@@ -1,18 +1,10 @@
 ﻿using HomeCenter.CodeGeneration;
 using HomeCenter.Model.Actors;
-using HomeCenter.Model.Components;
-using HomeCenter.Model.Core;
-using System.Collections.Generic;
 
 namespace HomeCenter.Model.Areas
 {
     [ProxyCodeGenerator]
     public class Area : DeviceActor
     {
-        [Map] private List<Area> _areas { get; set; } = new List<Area>();
-
-        [Map] private IList<Component> _components { get; set; } = new List<Component>();
-
-        public override string ToString() => $"{Uid} with {_components.Count} components";
     }
 }
