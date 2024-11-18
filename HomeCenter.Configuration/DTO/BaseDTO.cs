a28253e951131c8474f4606e8cc695b391a7e90c
Dominik Jeske <dominik.jeske@hotmail.com>
2024-11-18 22:14:23 +0100

    Update to net 8.0

--- HomeCenter.Configuration/DTO/BaseDTO.cs
+++ HomeCenter.Configuration/DTO/BaseDTO.cs
@@ -1,6 +1,5 @@
 ﻿using HomeCenter.Abstractions;
 using HomeCenter.EventAggregator;
-using HomeCenter.Extensions;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text.Json.Serialization;
